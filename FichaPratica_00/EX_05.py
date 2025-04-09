f3d07194ea7da2c2c7df8efbd0d0db645184023d
aws14 <aws14@CESAEPRT.WAN>
2025-04-09 10:00:40 +0100

    ex.5

--- FichaPratica_00/EX_05.py
+++ FichaPratica_00/EX_05.py
@@ -3,7 +3,7 @@ numero2 = float(input("numero 2: "))
 numero3 = float(input("numero 3: "))
 
 mediaAri = (numero1+numero2+numero3) / 3
-mediaPon = (numero1*0.20) + (numero2*0.30) + (numero3*0.50) / (0.20+0.30+0.50)
+mediaPon = (numero1*0.20) + (numero2*0.30) + (numero3*0.50) 
 
 print("Media Aritimetica", mediaAri)
 print("Media Ponderada", mediaPon)
