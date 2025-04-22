ab9d9a887196d449541ff4519d58de42f9ab49b0
patrickribeir <patricklk1@icloud.com>
2025-04-22 15:09:56 +0100

    update final

--- FichaPratica_02/EX_11.py
+++ FichaPratica_02/EX_11.py
@@ -1,29 +1,24 @@
-# 0 - 25
-contador_0_25 = 0
-# 26 - 50
-contador_26_50 = 0
-# 51 -75
-contador_51_75 = 0
-#76 - 100
-contador_76_100 = 0
 
-while True:
-    numIntervalo = int(input("insira um numero positivo e inteiro: "))
-    if numIntervalo < 0:
-        break
-    if 0 <= numIntervalo <= 25:
-        contador_0_25 += 1
-    elif 26 <= numIntervalo <= 50:
-        contador_26_50 += 1
-    elif 51 <= numIntervalo <= 75:
-        contador_51_75 += 1
-    elif 76 <= numIntervalo <= 100:
-        contador_76_100 += 1
-
-print ("[0.25]", contador_0_25)
-print ("[26.50]", contador_26_50)
-print ("[51.75]", contador_51_75)
-print ("[76.100]", contador_76_100)
-
-
-#Pedir ajuda 
+number=0
+interval0_25=0
+interval26_50=0
+interval51_75=0
+interval76_100=0
+  
+#Asks the user for a number and if the number is negative it ends
+#Sees in what interval these number is in and prints
+while number>=0:
+    number=int(input("Insert a number from 0 to 100. To finish enter a negative number: "))
+    print(number)
+    if number>=0 and number<=25:
+        interval0_25+=1
+    elif number>=26 and number<=50:
+        interval26_50+=1
+    elif number>=51 and number<=75:
+        interval51_75+=1
+    elif number>=76 and number<=100:
+        interval76_100+=1
+    else:
+        print("The number is outside the defined interval!")
+      
+print("[00, 25]: ",interval0_25,"\n[26, 50]: ",interval26_50,"\n[51, 75]: ",interval51_75,"\n[76, 100]: ",interval76_100)
